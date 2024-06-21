46cd979faa9f9c3295eb5134aa24847998660182
StanciuNA <nicu.stanciu38@gmail.com>
2024-06-21 10:25:45 +0200

    V0.0.5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomepageComponent } from './components/homepage/homepage.component'
-import { LoginComponent } from './components/login/login.component'
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
 import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
 
 export const routes: Routes = [
   { path: '', component:HomepageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
 ];
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,26 +1,47 @@
-import { Component,Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProductsService } from '../../services/products/products.service';
 import { CommonModule } from '@angular/common';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatPaginatorModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
+
 export class ProductsComponent {
+
+  current_page = 1
+
+  products_per_page = 10
 
   list_of_products = [];
 
+
   constructor(public productsService : ProductsService){
     this.getProducts().subscribe((data: any) => {
-      this.list_of_products = data;
-      console.log(this.list_of_products);
+      this.list_of_products = data.map((product: any) => {
+        return { ...product, quantity: 1 };
+      });
     });
   }
 
   getProducts(){
     return this.productsService.getProducts();
+  }
+
+  increment(product : any){
+    if (product['quantity'] < 99){
+      product.quantity++;
+    }
+  }
+
+  decrement(product : any){
+    if (product['quantity'] > 1){
+      product.quantity--;
+    }
   }
 }
